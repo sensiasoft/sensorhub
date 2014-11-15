05e0312df130acb70a03408b461dfa3de819e439
sensiasoft <alex.robin@sensiasoftware.com>
2014-11-16 00:12:33 +0100

    Fixed multithreading bug in video test

--- sensorhub-test/src/test/java/TestSensorHub.java
+++ sensorhub-test/src/test/java/TestSensorHub.java
@@ -2,6 +2,7 @@ import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.net.URLConnection;
@@ -10,12 +11,12 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import javax.swing.JFrame;
+import net.opengis.swe.v20.DataArray;
+import net.opengis.swe.v20.DataBlock;
 import org.sensorhub.impl.SensorHub;
 import org.sensorhub.impl.service.HttpServer;
 import org.sensorhub.ui.AdminUI;
-import org.vast.cdm.common.DataBlock;
 import org.vast.cdm.common.DataStreamParser;
-import org.vast.data.DataArray;
 import org.vast.ows.OWSUtils;
 import org.vast.ows.sos.GetResultRequest;
 import org.vast.ows.sos.GetResultTemplateRequest;
@@ -31,7 +32,7 @@ public class TestSensorHub
     {
         SensorHub.main(new String[] {"src/test/resources/sensorhub-config.json", "storage"});
         Thread.sleep(1000);
-        OWSUtils utils = new OWSUtils();
+        final OWSUtils utils = new OWSUtils();
         
         // deploy Vaadin servlet
         VaadinServlet vaadin = new VaadinServlet();
@@ -44,54 +45,65 @@ public class TestSensorHub
         GetResultTemplateRequest gtr = new GetResultTemplateRequest();
         gtr.setGetServer("http://localhost:8080/sensorhub/sos");
         gtr.setOffering("urn:mysos:offering01");
-        gtr.getObservables().add("http://myonto/def/videoFrame");
+        gtr.getObservables().add("http://sensorml.com/ont/swe/property/VideoFrame");
         gtr.setVersion("2.0");        
         URLConnection conn = utils.sendGetRequest(gtr);
         final GetResultTemplateResponse grtResp = (GetResultTemplateResponse)utils.readXMLResponse(conn.getInputStream(), OWSUtils.SOS, "GetResultTemplateResponse");
         final int height = grtResp.getResultStructure().getComponentCount();
-        final int width = ((DataArray)grtResp.getResultStructure()).getArrayComponent().getComponentCount();
+        final int width = ((DataArray)grtResp.getResultStructure()).getElementType().getComponentCount();
         utils.writeXMLResponse(System.out, grtResp);
         
         // get video data, parse it and display it
         final GetResultRequest gt = new GetResultRequest();
         gt.setGetServer("http://localhost:8080/sensorhub/sos");
         gt.setOffering("urn:mysos:offering01");
-        gt.getObservables().add("http://myonto/def/videoFrame");
-        gt.setVersion("2.0");                
-                
+        gt.getObservables().add("http://sensorml.com/ont/swe/property/VideoFrame");
+        gt.setVersion("2.0");
+        
         Runnable job = new Runnable() {
             
             public void run()
             {
                 try
                 {
-                    OWSUtils utils = new OWSUtils();
                     JFrame f = new JFrame("Video");
                     f.setSize(width, height);
                     f.setVisible(true);
                     BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
                     
-                    InputStream is = new BufferedInputStream(utils.sendGetRequest(gt).getInputStream());
+                    // we need to sync here because instantiation of GetResultWriter is not threadsafe
+                    // probably because of FES.getInstance() call
+                    InputStream is = null;
+                    synchronized(utils)
+                    {
+                        is = new BufferedInputStream(utils.sendGetRequest(gt).getInputStream());
+                    }
+                    
                     DataStreamParser parser = SWEFactory.createDataParser(grtResp.getResultEncoding());
                     parser.setDataComponents(grtResp.getResultStructure());
+                    parser.setRenewDataBlock(false);
                     parser.setInput(is);
-                    
+                                        
                     DataBlock data;
+                    WritableRaster raster = null;
                     long t0 = System.currentTimeMillis();
                     while ((data = parser.parseNextBlock()) != null)
                     {
-                        DataBufferByte buf = new DataBufferByte((byte[])data.getUnderlyingObject(), width);
-                        Raster raster = Raster.createRaster(img.getSampleModel(), buf, new Point(0,0));
-                        img.setData(raster);
-                        /*for (int i=0; i<data.getAtomCount(); i+=3)
+                        if (raster == null)
                         {
-                            int p = i/3;
-                            int v = p/width;
-                            int u = p%width;
-                            img.getRaster().setSample(u, v, 0, data.getIntValue(i));
-                            img.getRaster().setSample(u, v, 1, data.getIntValue(i+1));
-                            img.getRaster().setSample(u, v, 2, data.getIntValue(i+2));
-                        }*/          
+                            DataBufferByte buf = new DataBufferByte((byte[])data.getUnderlyingObject(), width);
+                            raster = Raster.createWritableRaster(img.getSampleModel(), buf, new Point(0,0));
+                            img = new BufferedImage(img.getColorModel(), raster, false, null);
+                        }
+//                        for (int i=0; i<data.getAtomCount(); i+=3)
+//                        {
+//                            int p = i/3;
+//                            int v = p/width;
+//                            int u = p%width;
+//                            img.getRaster().setSample(u, v, 0, data.getIntValue(i));
+//                            img.getRaster().setSample(u, v, 1, data.getIntValue(i+1));
+//                            img.getRaster().setSample(u, v, 2, data.getIntValue(i+2));
+//                        }          
                         f.getContentPane().getGraphics().drawImage(img, 0, 0, null);
                         long t1 = System.currentTimeMillis();
                         //System.out.println(t1 - t0);
