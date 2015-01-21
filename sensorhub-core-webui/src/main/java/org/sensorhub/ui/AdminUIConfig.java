e0b6278993f2b7c19664eb563f06efb84bbc3393
sensiasoft <alex.robin@sensiasoftware.com>
2015-01-21 08:46:28 +0100

    On-going mods to web UI

---
+++ sensorhub-core-webui/src/main/java/org/sensorhub/ui/AdminUIConfig.java
@@ -0,0 +1,36 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+The contents of this file are subject to the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the License.
+ 
+The Initial Developer is Sensia Software LLC. Portions created by the Initial
+Developer are Copyright (C) 2014 the Initial Developer. All Rights Reserved.
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.sensorhub.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.sensorhub.api.module.ModuleConfig;
+
+
+public class AdminUIConfig extends ModuleConfig
+{
+    private static final long serialVersionUID = 2000598898056182847L;
+    
+    
+    public List<CustomFormConfig> customForms = new ArrayList<CustomFormConfig>();
+    
+    
+    public AdminUIConfig()
+    {
+        this.name = "Admin UI";
+    }
+
+}
