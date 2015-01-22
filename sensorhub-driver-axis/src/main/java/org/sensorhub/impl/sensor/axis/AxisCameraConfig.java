3a4a4c3343afc86ce1e5626caf7a8e396e63cac6
mikebotts <mike.botts@botts-inc.com>
2015-01-22 16:45:38 -0600

    minor change

--- sensorhub-driver-axis/src/main/java/org/sensorhub/impl/sensor/axis/AxisCameraConfig.java
+++ sensorhub-driver-axis/src/main/java/org/sensorhub/impl/sensor/axis/AxisCameraConfig.java
@@ -38,4 +38,6 @@ public class AxisCameraConfig extends SensorConfig {
 	private static final long serialVersionUID = 5902668618197133068L;
 	
 	public String ipAddress = "192.168.1.50";
+	//public String ipAddress = "192.168.0.60";
+
 }
