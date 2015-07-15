1b1da7d298a7fc0833fa45c1b722963c008cb9f0
sensiasoft <alex.robin@sensiasoftware.com>
2015-07-15 18:44:20 +0200

    Added skeleton for new SensorThings API implementation

---
+++ sensorhub-service-sensorthings/src/main/java/org/sensorhub/impl/service/sensorthings/SensorThingsService.java
@@ -0,0 +1,91 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are Copyright (C) 2014 Sensia Software LLC.
+ All Rights Reserved.
+ 
+ Contributor(s): 
+    Alexandre Robin <alex.robin@sensiasoftware.com>
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.sensorhub.impl.service.sensorthings;
+
+import java.util.ArrayList;
+import org.sensorhub.api.common.SensorHubException;
+import org.sensorhub.api.persistence.IObsStorage;
+import org.sensorhub.impl.SensorHub;
+import org.sensorhub.impl.module.AbstractModule;
+import org.sensorhub.impl.module.ModuleRegistry;
+import org.sensorhub.impl.service.HttpServer;
+
+
+/**
+ * <p>
+ * Main module implementing SensorThings API.<br/>
+ * The real work is done in the servlet.
+ * </p>
+ *
+ * <p>Copyright (c) 2014 Sensia Software LLC</p>
+ * @author Alexandre Robin <alex.robin@sensiasoftware.com>
+ * @since Jul 15, 2015
+ */
+public class SensorThingsService extends AbstractModule<SensorThingsConfig>
+{
+    SensorThingsServlet servlet;
+    
+    
+    public SensorThingsService()
+    {        
+    }    
+
+
+    @Override
+    public void start() throws SensorHubException
+    {
+        if (!HttpServer.getInstance().isEnabled())
+            return;
+        
+        if (servlet == null)
+        {
+            // build list of connected storages
+            ModuleRegistry reg = SensorHub.getInstance().getModuleRegistry();
+            ArrayList<IObsStorage> storageList = new ArrayList<IObsStorage>();
+            for (String storageID: config.obsStorageIDs)
+            {
+                try
+                {
+                    storageList.add((IObsStorage)reg.getModuleById(storageID));
+                }
+                catch (Exception e)
+                {
+                    throw new SensorHubException("Cannot instantiate storage with ID " + storageID, e);
+                }
+            }
+            
+            // instantiate servlet with connected storage
+            servlet = new SensorThingsServlet(storageList);
+            
+            // deploy servlet to HTTP server
+            HttpServer.getInstance().deployServlet(servlet, config.endPoint);
+        }
+    }
+    
+
+    @Override
+    public void stop() throws SensorHubException
+    {
+        if (!HttpServer.getInstance().isEnabled())
+            return;
+        
+        HttpServer.getInstance().undeployServlet(servlet);
+        servlet = null;
+    }
+    
+
+    @Override
+    public void cleanup() throws SensorHubException
+    {
+        
+    }
+
+}
