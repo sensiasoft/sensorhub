9ca2978e6e28449b6c51a721ce3b97e78b7c80b8
sensiasoft <alex.robin@sensiasoftware.com>
2015-09-27 22:29:27 +0200

    Added logback config to main distribution so users can configure logging when needed

--- sensorhub-dist/src/scripts/launch.sh
+++ sensorhub-dist/src/scripts/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx128m -cp "lib/*" -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" org.sensorhub.impl.SensorHub config.json db
+java -Xmx128m -Dlogback.configurationFile=./logback.xml -cp "lib/*" -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" org.sensorhub.impl.SensorHub config.json db
