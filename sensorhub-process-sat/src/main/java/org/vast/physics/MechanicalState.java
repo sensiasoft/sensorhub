ed1089eeccd2a7af6be19f2fe842420ea4eb2e94
sensiasoft <alex.robin@sensiasoftware.com>
2015-10-16 00:13:41 +0200

    Moved processing stuff to separate repo

--- sensorhub-process-sat/src/main/java/org/vast/physics/MechanicalState.java
+++
@@ -1,43 +0,0 @@
-/***************************** BEGIN LICENSE BLOCK ***************************
-
- The contents of this file are subject to the Mozilla Public License Version
- 1.1 (the "License"); you may not use this file except in compliance with
- the License. You may obtain a copy of the License at
- http://www.mozilla.org/MPL/MPL-1.1.html
- 
- Software distributed under the License is distributed on an "AS IS" basis,
- WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- for the specific language governing rights and limitations under the License.
- 
- The Original Code is the "SensorML DataProcessing Engine".
- 
- The Initial Developer of the Original Code is the VAST team at the University of Alabama in Huntsville (UAH). <http://vast.uah.edu> Portions created by the Initial Developer are Copyright (C) 2007 the Initial Developer. All Rights Reserved. Please Contact Mike Botts <mike.botts@uah.edu> for more information.
- 
- Contributor(s): 
-    Alexandre Robin <robin@nsstc.uah.edu>
- 
-******************************* END LICENSE BLOCK ***************************/
-
-package org.vast.physics;
-
-import org.sensorhub.vecmath.Quat4d;
-import org.sensorhub.vecmath.Vect3d;
-
-
-public class MechanicalState
-{
-    public double julianTime;
-
-    public Vect3d linearPosition;
-    public Vect3d linearVelocity;
-    public Vect3d linearAcceleration;
-
-    public Quat4d angularPosition;
-    public Quat4d angularVelocity;
-    public Quat4d angularAcceleration;
-
-
-    public MechanicalState()
-    {
-    }
-}
